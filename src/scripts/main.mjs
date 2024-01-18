31d25dbc17f0a3144b78cd0dea8945bbef7681b5
Yuuji Hasegawa <hasegawa@kamenwriter.com>
2024-01-18 11:45:22 +0900

    bx-05 build

--- src/scripts/main.mjs
+++ src/scripts/main.mjs
@@ -7,9 +7,53 @@ lazyMap();
 import { shareForm } from "./shareForm.js";
 shareForm();
 
-/*
+import Swiper from "swiper";
+import { Autoplay, EffectFade } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/effect-fade";
 
-import { sidemenu } from "./sidemenu.js";
-sidemenu();
+const swiper = new Swiper(".swiper", {
+  modules: [Autoplay, EffectFade],
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  speed: 500,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    waitForTransition: false,
+  },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+});
 
-*/
+import "photoswipe/dist/photoswipe.css";
+import PhotoSwipeLightbox from "photoswipe/lightbox";
+const lightBox = new PhotoSwipeLightbox({
+  gallery: "#photos",
+  children: "a",
+  pswpModule: () => import("photoswipe"),
+});
+lightBox.init();
+document.addEventListener("DOMContentLoaded", function () {
+  const photos = document.querySelectorAll("#photos a");
+  photos.forEach((el) => {
+    loadImage(el.href).then((img) => {
+      el.setAttribute("data-pswp-width", img.naturalWidth);
+      el.setAttribute("data-pswp-height", img.naturalHeight);
+      el.firstElementChild.removeAttribute("style");
+    });
+  });
+});
+
+function loadImage(src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = (e) => reject(e);
+    img.src = src;
+  });
+}
